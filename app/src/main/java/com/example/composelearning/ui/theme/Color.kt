f936d5a20332ce693f551af0da720bf6d17c5005
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2022-06-22 23:28:47 +0530

    Custom Slider with image as thumb to please designers.

--- app/src/main/java/com/example/composelearning/ui/theme/Color.kt
+++ app/src/main/java/com/example/composelearning/ui/theme/Color.kt
@@ -7,3 +7,7 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
+val ThumbColor = Color(0xfff0f0f0)
+val ActiveTrackColor = Color(0xff489cef)
+val InactiveTrackColor = Color(0xffcccccc)
+
