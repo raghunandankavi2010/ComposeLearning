7345a44ad0448aa5ea8e699608ff01c48b67527e
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2022-06-27 16:12:54 +0530

    Icon with Border Button.

---
+++ app/src/main/java/com/example/composelearning/buttons/TintedIconButtonWithBorder.kt
@@ -0,0 +1,61 @@
+package com.example.composelearning.buttons
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.composelearning.ui.theme.DEFAULT060
+import com.example.composelearning.ui.theme.GRAY040
+import com.example.composelearning.ui.theme.GREY094
+
+@Composable
+fun TintedIconButtonWithBorder(
+    onClick: () -> Unit,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector,
+    strokeWidth: Dp = 2.dp,
+    borderColor: Color,
+    iconTintColor: Color
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    val color = Modifier.background(color = Color.White)
+    val border = Modifier.border(
+        width = strokeWidth,
+        color = borderColor,
+        shape = CircleShape
+    )
+    Surface(
+        modifier = modifier
+            .clickable(
+                onClick = onClick,
+                interactionSource = interactionSource,
+                indication = null
+            )
+            .clip(CircleShape)
+            .then(border)
+            .then(Modifier.background(Color.White)),
+        color = Color.Transparent
+    ) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = contentDescription,
+            modifier = color,
+            tint = iconTintColor
+        )
+    }
+}
