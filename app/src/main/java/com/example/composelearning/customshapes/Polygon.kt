94dc7c20d4d079de689a4fdfc7a3413a30924958
Raghunandan <raghunandan2005@gmail.com>
2022-06-30 13:05:09 +0530

    Add comments on the geometry.

--- app/src/main/java/com/example/composelearning/customshapes/Polygon.kt
+++ app/src/main/java/com/example/composelearning/customshapes/Polygon.kt
@@ -7,8 +7,8 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
-import java.lang.Math.cos
-import java.lang.Math.sin
+import kotlin.math.cos
+import kotlin.math.sin
 
 class Polygon(val sides: Int, val rotation: Float = 0f) : Shape {
     override fun createOutline(
@@ -18,27 +18,28 @@ class Polygon(val sides: Int, val rotation: Float = 0f) : Shape {
     ): Outline {
         return Outline.Generic(
             Path().apply {
+                // calculate radius
                 val radius = if (size.width > size.height) size.width / 2f else size.height / 2f
-                Log.i("Polygon","Radius $radius")
+                // calculate 180/number of sides in radians
                 val angle = 2.0 * Math.PI / sides
-                Log.i("Polygon","$angle")
+                // center of the shape
                 val cx = size.width / 2f
                 val cy = size.height / 2f
-                Log.i("Polygon","$cx $cy")
+                // in case you need to rotate
                 val r = rotation * (Math.PI / 180)
-                Log.i("Polygon","$r")
+                // move to the right its like calculating a point on the circumference of the circle
                 moveTo(
-                    cx + (radius * kotlin.math.cos(0.0 + r).toFloat()),
+                    cx + (radius * cos(0.0 + r).toFloat()),
                     cy + (radius * sin(0.0 + r).toFloat())
                 )
-                Log.i("Polygon x","${cx + (radius * cos(0.0 + r).toFloat())}")
-                Log.i("Polygon y","${cy + (radius * sin(0.0 + r).toFloat())}")
+                // calculate the other points using the same logic
                 for (i in 1 until sides) {
                     lineTo(
                         cx + (radius * cos(angle * i + r).toFloat()),
                         cy + (radius * sin(angle * i + r).toFloat())
                     )
                 }
+                // close the path
                 close()
             })
     }
