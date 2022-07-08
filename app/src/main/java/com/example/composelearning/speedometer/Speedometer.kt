2aa3491dcde6a6a6faaf3958b4652b5b07342ac8
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2022-07-08 17:48:36 +0530

    Adding text and markers as well for speedometer.

--- app/src/main/java/com/example/composelearning/speedometer/Speedometer.kt
+++ app/src/main/java/com/example/composelearning/speedometer/Speedometer.kt
@@ -1,9 +1,16 @@
 package com.example.composelearning.speedometer
 
 import android.annotation.SuppressLint
-import androidx.compose.animation.core.*
+import android.graphics.Paint
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Slider
 import androidx.compose.material.Text
@@ -12,10 +19,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import kotlin.math.PI
@@ -24,21 +31,8 @@ import kotlin.math.sin
 
 @Composable
 fun SpeedometerScreen() {
-    var targetValue by remember {
-        mutableStateOf(0f)
-    }
-    val progress = remember { mutableStateOf(0f) }
     Column(Modifier.padding(16.dp)) {
-        Slider(value = targetValue, onValueChangeFinished = {
-        }, onValueChange = { targetValue = it }, valueRange = 0f..300f)
-
-        Text(text = "${targetValue.toInt()}")
-        Button(onClick = {
-            progress.value = targetValue
-        }) {
-            Text(text = "Reset")
-        }
-        Speedometer(progress.value.toInt())
+        Speedometer(50)
     }
 }
 
@@ -50,17 +44,16 @@ fun Speedometer(
     val arcDegrees = 275
     val startArcAngle = 135f
     val startAngleRadians = startArcAngle * (PI / 180f)
-    val progressInRadians = (progress * (arcDegrees) / 300f) * (PI / 180f)
+    val progressInRadians = (progress * (arcDegrees) / 100f) * (PI / 180f)
     val endProgressInRadians = startAngleRadians + progressInRadians
+    // remember
+    val progressAnimation = remember {
+        Animatable(0f)
+    }
+    val pointerAnimation = remember {
+        Animatable(startAngleRadians.toFloat())
+    }
 
-    // remember is not used so as to restart the animation from start
-    val progressAnimation = Animatable(0f)
-    val pointerAnimation = Animatable(startAngleRadians.toFloat())
-
-
-    /** restart @LaunchedEffect
-     *  when progress changes
-     * **/
     LaunchedEffect(progress) {
         launch {
             pointerAnimation.animateTo(
@@ -93,7 +86,6 @@ fun Speedometer(
                     else -> // Green
                         Color(0xFF388E3C) to Color(0xFFC8E6C9)
                 }
-
                 val centerArcSize = Size(w / 2f, h / 2f)
                 val centerArcStroke = Stroke(20f, 0f, StrokeCap.Round)
                 drawArc(
@@ -109,7 +101,7 @@ fun Speedometer(
                 drawArc(
                     mainColor,
                     startArcAngle,
-                    progressAnimation.value * (arcDegrees) / 300f,
+                    progressAnimation.value * (arcDegrees) / 100f,
                     false,
                     topLeft = quarterOffset,
                     size = centerArcSize,
@@ -117,14 +109,50 @@ fun Speedometer(
                 )
                 // Drawing the pointer circle
                 drawCircle(mainColor, 20f, centerOffset)
-                println("Values ${pointerAnimation.value}")
                 // draw pointer
                 val r = w / 4f
+                val x = (r - 30f) * cos(pointerAnimation.value) + w / 2
+                val y = (r - 30f) * sin(pointerAnimation.value) + h / 2
+                println("Times Called")
+                drawLine(mainColor, Offset(w / 2, h / 2), Offset(x, y), 20f, StrokeCap.Round)
+                var markerAngleRadians = 0.0
+                var textValue = 0
+                var angleStart = 135f
+                val angleEnd = 410f
 
-                val x = r * cos(pointerAnimation.value) + w / 2
-                val y = r * sin(pointerAnimation.value) + h / 2
+                // draw markers
+                while(angleStart <= angleEnd) {
+                    markerAngleRadians = angleStart * (PI / 180f)
+                    //  println(i)
+                    val x1 = (r - 10f) * cos(markerAngleRadians) + w / 2
+                    val y1 = (r - 10f) * sin(markerAngleRadians) + h / 2
+                    val x2 = (r + 10f) * cos(markerAngleRadians) + w / 2
+                    val y2 = (r + 10f) * sin(markerAngleRadians) + h / 2
+                    val tx = (r + 40f) * cos(markerAngleRadians) + w / 2
+                    val ty = (r + 40f) * sin(markerAngleRadians) + h / 2
 
-                drawLine(mainColor,Offset(w/2, h/2), Offset(x, y), 20f, StrokeCap.Round)
+                    drawContext.canvas.nativeCanvas.apply {
+                        drawText(
+                            "$textValue",
+                            tx.toFloat(),
+                            ty.toFloat(),
+                            Paint().apply {
+                                textSize = 20f
+                                color = android.graphics.Color.BLACK
+                                textAlign = Paint.Align.CENTER
+                            }
+                        )
+                    }
+                    textValue += 10
+
+                    drawLine(Color.Black,
+                        Offset(x1.toFloat(), y1.toFloat()),
+                        Offset(x2.toFloat(), y2.toFloat()),
+                        5f,
+                        StrokeCap.Square)
+
+                    angleStart += 27.5f
+                }
             }
         }
     )
