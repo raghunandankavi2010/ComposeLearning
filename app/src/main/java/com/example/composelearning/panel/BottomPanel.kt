9dde708d0c5053ec4ef648f7f1270ed14677fad9
Raghunandan <raghunandan2005@gmail.com>
2022-07-14 22:46:59 +0530

    Just trying to make sure elevation works by drawing other content below the bottom panel

--- app/src/main/java/com/example/composelearning/panel/BottomPanel.kt
+++ app/src/main/java/com/example/composelearning/panel/BottomPanel.kt
@@ -2,16 +2,18 @@ package com.example.composelearning.panel
 
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
+import com.example.composelearning.speedometer.Speedometer
 
 @Composable
 fun BottomPanel() {
 
-    Column(modifier = Modifier) {
-
+    Column(modifier = Modifier.fillMaxSize()) {
         BoxWithConstraints {
+            Speedometer(progress = 100)
             val boxHeight = with(LocalDensity.current) { constraints.maxHeight.toDp() }
             Content(
                 boxHeight = boxHeight
