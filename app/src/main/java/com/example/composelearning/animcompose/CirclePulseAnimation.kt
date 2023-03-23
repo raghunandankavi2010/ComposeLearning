b808ef09e5e9fc2402edd4481f73fbd3cd43ee81
raghu <raghunandankavi2010@gmail.com>
2023-03-23 09:05:13 +0530

    Pulsating circle animation compose android

--- app/src/main/java/com/example/composelearning/animcompose/CirclePulseAnimation.kt
+++ app/src/main/java/com/example/composelearning/animcompose/CirclePulseAnimation.kt
@@ -5,13 +5,18 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
+import com.example.composelearning.customshapes.dpToPx
 
 @Composable
 fun PulsatingCircle() {
+    val strokeWidth by remember { mutableStateOf(10) }
     val infiniteTransition = rememberInfiniteTransition()
     val pulseMagnitude by infiniteTransition.animateFloat(
         initialValue = 0f,
@@ -24,9 +29,15 @@ fun PulsatingCircle() {
 
     Canvas(modifier = Modifier.fillMaxSize()) {
         drawCircle(
-            color = Color.Red.copy(alpha = 0.5f),
-            radius = size.minDimension / 2 * pulseMagnitude - 10.dp.toPx(),
-            style = Stroke(width = 10.dp.toPx())
+            color = Color.Red.copy(alpha = 0.2f),
+            radius = size.minDimension / 2 * pulseMagnitude - strokeWidth.dp.toPx(),
+            style = Fill
+        )
+
+        drawCircle(
+            color = Color.Green.copy(alpha = 1f),
+            radius = size.minDimension / 2 * pulseMagnitude - strokeWidth.dp.toPx(),
+            style = Stroke(width = strokeWidth.dp.toPx())
         )
     }
 }
