ecfdd4835a9970292bb1c571204a4f1415b70e0e
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2023-07-17 22:15:57 +0530

    Boring compose stuff for sake of learning.

---
+++ app/src/main/java/com/example/composelearning/lists/LazyRowAnim.kt
@@ -0,0 +1,33 @@
+package com.example.composelearning.lists
+
+
+//import androidx.compose.foundation.background
+//import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.unit.dp
+//
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun EquiRow() {
+//    Row(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(16.dp),
+//        horizontalArrangement = Arrangement.spacedBy(16.dp)
+//    ) {
+//        repeat(4) {
+//            Box(
+//                modifier = Modifier
+//                    .width(50.dp)
+//                    .height(50.dp)
+//                    .background(Color.Blue)
+//            ) {
+//
+//            }
+//        }
+//    }
+//}
