c4c700b70f7169ad17cacb02222ce6997e9e72b5
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2024-01-05 15:21:22 +0530

    Minor Changes.

--- app/src/main/java/com/example/composelearning/lists/JioNews.kt
+++ app/src/main/java/com/example/composelearning/lists/JioNews.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -73,8 +72,7 @@ fun JioNews() {
                 fontSize = 14.sp,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .weight(1f)
-                    .weight(1f, false)
+                    .weight(1f, true)
             )
 
             TextHeaderFooter(
