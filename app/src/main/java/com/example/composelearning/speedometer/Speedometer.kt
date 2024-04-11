6875ddec273dc8318f80efe1c5426c6ca6e7a5c4
Raghunandan K <raghunandankavi2010@gmail.com>
2024-04-11 22:05:01 +0530

    Measure text before drawing

--- app/src/main/java/com/example/composelearning/speedometer/Speedometer.kt
+++ app/src/main/java/com/example/composelearning/speedometer/Speedometer.kt
@@ -12,7 +12,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
@@ -23,17 +22,11 @@ import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.drawText
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.rememberTextMeasurer
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.content.res.ResourcesCompat
 import com.example.composelearning.LogCompositions
 import com.example.composelearning.R
 import kotlinx.coroutines.launch
@@ -64,101 +57,106 @@ fun Speedometer(
         launch {
             pointerAnimation.animateTo(
                 targetValue = endProgressInRadians.toFloat(),
-                animationSpec = tween(durationMillis = 3000, easing = CustomEaseOutBounce))
+                animationSpec = tween(durationMillis = 3000, easing = CustomEaseOutBounce)
+            )
         }
         launch {
             progressAnimation.animateTo(
                 targetValue = progress.toFloat(),
-                animationSpec = tween(durationMillis = 3000, easing = CustomEaseOutBounce))
+                animationSpec = tween(durationMillis = 3000, easing = CustomEaseOutBounce)
+            )
         }
     }
 
     Canvas(
         modifier = Modifier
             .fillMaxWidth()
-            .aspectRatio(1f),
-        onDraw = {
-            drawIntoCanvas {
-                val w = drawContext.size.width
-                val h = drawContext.size.height
-                val centerOffset = Offset(w / 2f, h / 2f)
-                val quarterOffset = Offset(w / 4f, h / 4f)
-                // Drawing Center Arc background
-                val (mainColor, secondaryColor) = when {
-                    progressAnimation.value < 50 -> // Red
-                        Color(0xFFD32F2F) to Color(0xFFFFCDD2)
-                    progressAnimation.value < 80 -> // Orange
-                        Color(0xFFF57C00) to Color(0xFFFFE0B2)
-                    else -> // Green
-                        Color(0xFF388E3C) to Color(0xFFC8E6C9)
+            .aspectRatio(1f)
+    ) {
+        drawIntoCanvas {
+            val w = drawContext.size.width
+            val h = drawContext.size.height
+            val centerOffset = Offset(w / 2f, h / 2f)
+            val quarterOffset = Offset(w / 4f, h / 4f)
+            // Drawing Center Arc background
+            val (mainColor, secondaryColor) = when {
+                progressAnimation.value < 50 -> // Red
+                    Color(0xFFD32F2F) to Color(0xFFFFCDD2)
+
+                progressAnimation.value < 80 -> // Orange
+                    Color(0xFFF57C00) to Color(0xFFFFE0B2)
+
+                else -> // Green
+                    Color(0xFF388E3C) to Color(0xFFC8E6C9)
+            }
+            val centerArcSize = Size(w / 2f, h / 2f)
+            val centerArcStroke = Stroke(20f, 0f, StrokeCap.Butt)
+            drawArc(
+                secondaryColor,
+                startArcAngle,
+                arcDegrees.toFloat(),
+                false,
+                topLeft = quarterOffset,
+                size = centerArcSize,
+                style = centerArcStroke
+            )
+            // Drawing Center Arc progress
+            drawArc(
+                mainColor,
+                startArcAngle,
+                progressAnimation.value * (arcDegrees) / 100f,
+                false,
+                topLeft = quarterOffset,
+                size = centerArcSize,
+                style = centerArcStroke
+            )
+            // Drawing the pointer circle
+            drawCircle(mainColor, 20f, centerOffset)
+            // draw pointer
+            val r = w / 4f
+            val x = (r - 30f) * cos(pointerAnimation.value) + w / 2
+            val y = (r - 30f) * sin(pointerAnimation.value) + h / 2
+
+            drawLine(mainColor, Offset(w / 2, h / 2), Offset(x, y), 20f, StrokeCap.Round)
+            var markerAngleRadians: Double
+            var textValue = 0
+            var angleStart = 135f
+            val angleEnd = 410f
+            // draw markers
+            while (angleStart <= angleEnd) {
+                markerAngleRadians = angleStart * (PI / 180f)
+                val x1 = (r - 20f) * cos(markerAngleRadians) + w / 2
+                val y1 = (r - 20f) * sin(markerAngleRadians) + h / 2
+                val x2 = (r + 20f) * cos(markerAngleRadians) + w / 2
+                val y2 = (r + 20f) * sin(markerAngleRadians) + h / 2
+                val tx = (r + 40f) * cos(markerAngleRadians) + w / 2
+                val ty = (r + 40f) * sin(markerAngleRadians) + h / 2
+
+                drawContext.canvas.nativeCanvas.apply {
+                    drawText(
+                        "$textValue",
+                        tx.toFloat(),
+                        ty.toFloat(),
+                        Paint().apply {
+                            textSize = 20f
+                            color = android.graphics.Color.BLACK
+                            textAlign = Paint.Align.CENTER
+                        }
+                    )
                 }
-                val centerArcSize = Size(w / 2f, h / 2f)
-                val centerArcStroke = Stroke(20f, 0f, StrokeCap.Butt)
-                drawArc(
-                    secondaryColor,
-                    startArcAngle,
-                    arcDegrees.toFloat(),
-                    false,
-                    topLeft = quarterOffset,
-                    size = centerArcSize,
-                    style = centerArcStroke
+                textValue += 10
+                drawLine(
+                    Color.Black,
+                    Offset(x1.toFloat(), y1.toFloat()),
+                    Offset(x2.toFloat(), y2.toFloat()),
+                    5f,
+                    StrokeCap.Square
                 )
-                // Drawing Center Arc progress
-                drawArc(
-                    mainColor,
-                    startArcAngle,
-                    progressAnimation.value * (arcDegrees) / 100f,
-                    false,
-                    topLeft = quarterOffset,
-                    size = centerArcSize,
-                    style = centerArcStroke
-                )
-                // Drawing the pointer circle
-                drawCircle(mainColor, 20f, centerOffset)
-                // draw pointer
-                val r = w / 4f
-                val x = (r - 30f) * cos(pointerAnimation.value) + w / 2
-                val y = (r - 30f) * sin(pointerAnimation.value) + h / 2
 
-                drawLine(mainColor, Offset(w / 2, h / 2), Offset(x, y), 20f, StrokeCap.Round)
-                var markerAngleRadians: Double
-                var textValue = 0
-                var angleStart = 135f
-                val angleEnd = 410f
-                // draw markers
-                while (angleStart <= angleEnd) {
-                    markerAngleRadians = angleStart * (PI / 180f)
-                    val x1 = (r - 20f) * cos(markerAngleRadians) + w / 2
-                    val y1 = (r - 20f) * sin(markerAngleRadians) + h / 2
-                    val x2 = (r + 20f) * cos(markerAngleRadians) + w / 2
-                    val y2 = (r + 20f) * sin(markerAngleRadians) + h / 2
-                    val tx = (r + 40f) * cos(markerAngleRadians) + w / 2
-                    val ty = (r + 40f) * sin(markerAngleRadians) + h / 2
-
-                    drawContext.canvas.nativeCanvas.apply {
-                        drawText(
-                            "$textValue",
-                            tx.toFloat(),
-                            ty.toFloat(),
-                            Paint().apply {
-                                textSize = 20f
-                                color = android.graphics.Color.BLACK
-                                textAlign = Paint.Align.CENTER
-                            }
-                        )
-                    }
-                    textValue += 10
-                    drawLine(Color.Black,
-                        Offset(x1.toFloat(), y1.toFloat()),
-                        Offset(x2.toFloat(), y2.toFloat()),
-                        5f,
-                        StrokeCap.Square)
-
-                    angleStart += 27.5f
-                }
+                angleStart += 27.5f
             }
         }
-    )
+    }
 }
 
 val EaseOutBounce: Easing = Easing { fraction ->
@@ -194,6 +192,8 @@ fun Speedometer3(
     progress: Int,
 ) {
     LogCompositions("Speedometer", "Running")
+    val textMeasurer = rememberTextMeasurer()
+
     val arcDegrees = 180f
     val startArcAngle = 180f
     val startAngleRadians = startArcAngle * (PI / 180f)
@@ -207,31 +207,24 @@ fun Speedometer3(
         Animatable(startAngleRadians.toFloat())
     }
 
-    val context = LocalContext.current
-
-    val fontFamily = remember {
-        FontFamily(
-            typeface = ResourcesCompat.getFont(context, R.font.jio_type_medium)!!
-        )
-    }
-
     LaunchedEffect(progress) {
         launch {
             pointerAnimation.animateTo(
                 targetValue = endProgressInRadians.toFloat(),
-                animationSpec = tween(durationMillis = 3000, easing = CustomEaseOutBounce))
+                animationSpec = tween(durationMillis = 3000, easing = CustomEaseOutBounce)
+            )
         }
         launch {
             progressAnimation.animateTo(
                 targetValue = progress.toFloat(),
-                animationSpec = tween(durationMillis = 3000, easing = CustomEaseOutBounce))
+                animationSpec = tween(durationMillis = 3000, easing = CustomEaseOutBounce)
+            )
         }
     }
 
+
     val vector = ImageVector.vectorResource(id = R.drawable.pointer_black)
     val painter = rememberVectorPainter(image = vector)
-
-    val textMeasurer = rememberTextMeasurer()
 
     Canvas(
         modifier = Modifier
@@ -252,7 +245,7 @@ fun Speedometer3(
                     false,
                     topLeft = quarterOffset,
                     size = centerArcSize,
-                    style =  Stroke(80f, 0f, StrokeCap.Round)
+                    style = Stroke(80f, 0f, StrokeCap.Round)
                 )
 
 
@@ -269,7 +262,7 @@ fun Speedometer3(
 
                 drawArc(
                     Color.Green,
-                    startArcAngle + 45 ,
+                    startArcAngle + 45,
                     25 * (arcDegrees) / 100f,
                     false,
                     topLeft = quarterOffset,
@@ -279,7 +272,7 @@ fun Speedometer3(
 
                 drawArc(
                     Color.Blue,
-                    startArcAngle + 90 ,
+                    startArcAngle + 90,
                     25 * (arcDegrees) / 100f,
                     false,
                     topLeft = quarterOffset,
@@ -297,26 +290,29 @@ fun Speedometer3(
 //                    style = centerArcStroke
 //                )
                 // Drawing the pointer circle
-               rotate(progressAnimation.value * (arcDegrees) / 100f , pivot = Offset(centerOffset.x,centerOffset.y)) {
+                rotate(
+                    progressAnimation.value * (arcDegrees) / 100f,
+                    pivot = Offset(centerOffset.x, centerOffset.y)
+                ) {
                     translate(
-                        left = centerOffset.x  - 125.dp.toPx(),
-                        top = centerOffset.y -  11.dp.toPx()
+                        left = centerOffset.x - 125.dp.toPx(),
+                        top = centerOffset.y - 11.dp.toPx()
                     ) {
                         with(painter) {
                             draw(
-                                size = Size(129.dp.toPx(),22.dp.toPx())
+                                size = Size(129.dp.toPx(), 22.dp.toPx())
                             )
                         }
                     }
                 }
 
-               // drawCircle(Color.Black, 20f, centerOffset)
+                //drawCircle(Color.Black, 5f, centerOffset)
                 // draw pointer
                 val r = w / 4f
                 val x = (r - 30f) * cos(pointerAnimation.value) + w / 2
                 val y = (r - 30f) * sin(pointerAnimation.value) + h / 2
 
-               // drawLine(Color.Black, Offset(w / 2, h / 2), Offset(x, y), 20f, StrokeCap.Round)
+                // drawLine(Color.Black, Offset(w / 2, h / 2), Offset(x, y), 20f, StrokeCap.Round)
                 var markerAngleRadians: Double
                 var textValue = 0
                 var angleStart = 180f
@@ -325,63 +321,55 @@ fun Speedometer3(
                 while (angleStart <= angleEnd) {
                     markerAngleRadians = angleStart * (PI / 180f)
                     val x1 = (r + 60f) * cos(markerAngleRadians) + w / 2
-                    val y1 = (r  + 60f) * sin(markerAngleRadians) + h / 2
+                    val y1 = (r + 60f) * sin(markerAngleRadians) + h / 2
                     val x2 = (r + 80f) * cos(markerAngleRadians) + w / 2
                     val y2 = (r + 80f) * sin(markerAngleRadians) + h / 2
-                    val tx = (r + 100f) * cos(markerAngleRadians) + w / 2
-                    val ty = (r + 100f) * sin(markerAngleRadians) + h / 2
+                    val tx = (r + 120f) * cos(markerAngleRadians) + w / 2
+                    val ty = (r + 120f) * sin(markerAngleRadians) + h / 2
 
+                    val textLayoutResult = textMeasurer.measure(
+                        AnnotatedString("$textValue")
+                    )
 
-                   val rotationAngle = if(textValue == 0 || textValue == 180 || textValue == 90) {
-                         0f
-                    } else {
-                        -45f
-                   }
-
-//                    rotate(rotationAngle , pivot = Offset(tx.toFloat(),ty.toFloat())) {
-//                        //translate(left = tx.toFloat() / 2, top = ty.toFloat() / 2) {
-//                            drawText(
-//                                textMeasurer,
-//                                text = "$textValue",
-//                                topLeft = Offset(tx.toFloat()  , ty.toFloat()),
-//                                style = TextStyle(fontSize = 16.sp, fontFamily = fontFamily)
-//                            )
-//                        }
-//                   // }
+                    val tSize = textLayoutResult.size
+                    //rotate(rotationAngle, pivot = Offset(tx.toFloat(), ty.toFloat())) {
+                    drawText(
+                        textMeasurer = textMeasurer,
+                        text = "$textValue",
+                        topLeft = Offset(
+                            tx.toFloat() - (tSize.width) / 2f,
+                            ty.toFloat() - (tSize.height) / 2f
+                        ),
+                    )
 
                     //rotate(rotationAngle , pivot = Offset(tx.toFloat(),ty.toFloat())) {
-                        drawContext.canvas.nativeCanvas.apply {
-                            drawText(
-                                "$textValue",
-                                tx.toFloat(),
-                                ty.toFloat(),
-                                Paint().apply {
-                                    textSize = 20.sp.toPx()
-                                    color = android.graphics.Color.BLACK
-                                    textAlign = Paint.Align.CENTER
-                                }
-                            )
-                        }
-                  // }
+//                    drawContext.canvas.nativeCanvas.apply {
+//                        drawText(
+//                            "$textValue",
+//                            tx.toFloat() - (tSize.width) / 2f,
+//                            ty.toFloat() ,
+//                            Paint().apply {
+//                                textSize = 20.sp.toPx()
+//                                color = android.graphics.Color.BLACK
+//                                textAlign = Paint.Align.CENTER
+//                            }
+//                        )
+//                    }
+                    // }
                     textValue += 45
-                    drawLine(Color.Black,
+                    drawLine(
+                        Color.Black,
                         Offset(x1.toFloat(), y1.toFloat()),
                         Offset(x2.toFloat(), y2.toFloat()),
                         5f,
-                        StrokeCap.Square)
+                        StrokeCap.Square
+                    )
 
                     angleStart += 45
                 }
             }
         }
     )
-}
-
-@Composable
-fun measureTextWidth(text: String, style: TextStyle): Dp {
-    val textMeasurer = rememberTextMeasurer()
-    val widthInPixels = textMeasurer.measure(text, style).size.width
-    return with(LocalDensity.current) { widthInPixels.toDp() }
 }
 
 
