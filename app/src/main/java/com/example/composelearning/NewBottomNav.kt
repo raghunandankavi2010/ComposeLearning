f3ef493229acefce3749ef47157e92b49cccb832
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2024-09-14 07:49:23 +0530

    Minor changes.

--- app/src/main/java/com/example/composelearning/NewBottomNav.kt
+++ app/src/main/java/com/example/composelearning/NewBottomNav.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -22,9 +23,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -43,7 +49,7 @@ fun SimpleJetsnackApp() {
                     CompositionLocalProvider(
                         LocalNavAnimatedVisibilityScope provides this
                     ) {
-                        HomeScreen(onSnackSelected = { navController.navigate("detail") })
+                        HomeScreen(navController,onSnackSelected = { navController.navigate("detail") })
                     }
                 }
                 composable(
@@ -56,16 +62,17 @@ fun SimpleJetsnackApp() {
     }
 }
 
-
-
-
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
-fun HomeScreen(onSnackSelected: () -> Unit) {
+fun HomeScreen(navController: NavHostController, onSnackSelected: () -> Unit) {
     val sharedTransitionScope = LocalSharedTransitionScope.current
         ?: throw IllegalStateException("No SharedElementScope found")
     val animatedVisibilityScope = LocalNavAnimatedVisibilityScope.current
         ?: throw IllegalStateException("No SharedElementScope found")
+
+    var selectedTab by remember { mutableStateOf("home") }
+
+
     Scaffold(
         bottomBar = {
             with(animatedVisibilityScope) {
@@ -77,14 +84,24 @@ fun HomeScreen(onSnackSelected: () -> Unit) {
                         NavigationBarItem(
                             icon = { Icon(Icons.Filled.Home, contentDescription = "Home") },
                             label = { Text("Home") },
-                            selected = true, // You'll need to manage the selected state
-                            onClick = { /*TODO*/ }
+                            selected = selectedTab == "home", // You'll need to manage the selected state
+                            onClick = {
+                                if (selectedTab != "home ") {
+                                    selectedTab = "home"
+                                    //navController.navigate("home")
+                                }
+                            }
                         )
                         NavigationBarItem(
                             icon = { Icon(Icons.Filled.Settings, contentDescription = "Settings") },
                             label = { Text("Settings") },
-                            selected = false, // You'll need to manage the selected state
-                            onClick = { /*TODO*/ }
+                            selected = selectedTab == "settings", // You'll need to manage the selected state
+                            onClick = {
+                                if (selectedTab != "settings ") {
+                                    selectedTab = "settings"
+                                    //navController.navigate("settings")
+                                }
+                            }
                         )
                     }
                 }
@@ -92,18 +109,25 @@ fun HomeScreen(onSnackSelected: () -> Unit) {
         }
     ) { innerPadding ->
 
-            Column(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Button(onClick = onSnackSelected) {
-                    Text("Go to Detail")
+        when (selectedTab) {
+            "home" -> {
+                Column(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = { navController.navigate("detail") }) {
+                        Text("Go to Detail")
+                    }
                 }
             }
 
+            "settings" -> {
+                SettingsScreen(innerPadding)
+            }
+        }
     }
 }
 
@@ -120,6 +144,20 @@ fun DetailScreen() {
     }
 }
 
+@Composable
+fun SettingsScreen(innerPadding: PaddingValues) {
+    Box(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize()
+            .background(Color.LightGray),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Settings Screen")
+    }
+}
+
 val LocalNavAnimatedVisibilityScope = compositionLocalOf<AnimatedVisibilityScope?> { null }
+
 @OptIn(ExperimentalSharedTransitionApi::class)
 val LocalSharedTransitionScope = compositionLocalOf<SharedTransitionScope?> { null }
