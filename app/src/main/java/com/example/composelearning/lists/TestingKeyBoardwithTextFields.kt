86aa43ed2e6b14993319fcf85b3962629a492ffb
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2025-03-20 11:02:05 +0530

    Adding toolbar and icon

--- app/src/main/java/com/example/composelearning/lists/TestingKeyBoardwithTextFields.kt
+++ app/src/main/java/com/example/composelearning/lists/TestingKeyBoardwithTextFields.kt
@@ -10,12 +10,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.relocation.BringIntoViewRequester
 import androidx.compose.foundation.relocation.bringIntoViewRequester
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
@@ -24,9 +29,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -36,6 +40,21 @@ import kotlinx.coroutines.launch
 @Composable
 fun TestTextFieldList() {
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("TextField List") },
+                navigationIcon = {
+                    IconButton(onClick = { /* Handle back button */ }) {
+                        Icon(Icons.Filled.Home, contentDescription = "Home")
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.LightGray,
+                    titleContentColor = Color.Black,
+                    navigationIconContentColor = Color.Black
+                )
+            )
+        },
         content = { paddingValues ->
             val state = rememberLazyListState()
             val textFieldValues = remember { mutableStateMapOf<Int, String>() }
@@ -63,7 +82,6 @@ fun YourTextField(index: Int, textFieldValues: MutableMap<Int, String>) {
     val bringIntoViewRequester = remember { BringIntoViewRequester() }
     val coroutineScope = rememberCoroutineScope()
     var text by remember { mutableStateOf("") }
-    val focusRequester = remember { FocusRequester() }
 
     Column {
         TextField(
@@ -73,7 +91,6 @@ fun YourTextField(index: Int, textFieldValues: MutableMap<Int, String>) {
                 text = it
             },
             modifier = Modifier
-                .focusRequester(focusRequester)
                 .bringIntoViewRequester(bringIntoViewRequester)
                 .onFocusEvent { focusState ->
                     if (focusState.isFocused) {
@@ -84,8 +101,6 @@ fun YourTextField(index: Int, textFieldValues: MutableMap<Int, String>) {
                 }
         )
         Text(text = "Value: ${textFieldValues[index] ?: ""}")
-        LaunchedEffect(Unit) {
-            focusRequester.requestFocus()
-        }
+
     }
 }
